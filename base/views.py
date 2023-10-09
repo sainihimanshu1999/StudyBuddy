bbe627710b3a43c2531a314588577af4f5f8bd2f
sainihimanshu1999 <sainihimanshu.1999@gmail.com>
2023-10-09 09:30:53 +0530

    messages functionality done

--- base/views.py
+++ base/views.py
@@ -77,7 +77,19 @@ def home(request):
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
-    context = {'room':room}
+    roomMessages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room',pk = room.id)
+
+
+    context = {'room':room , 'roomMessages':roomMessages, 'participants':participants}
     return render(request,'base/room.html',context)
 
 
@@ -121,3 +133,15 @@ def deleteRoom(request,pk):
         room.delete()
         return redirect('home')
     return render(request,'base/delete.html', {'obj':room})
+
+@login_required(login_url='login')
+def deleteMessage(request,pk):
+    message= Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here')
+
+    if request.method == "POST":
+        message.delete()
+        return redirect('home')
+    return render(request,'base/delete.html', {'obj':message})
